02b55a4822c9acb26205c5aafd496bfd19462b47
Xingzhong <xingzhong@me.com>
2024-03-30 13:28:49 +0800

    update region list

--- pages/api/proxy.ts
+++ pages/api/proxy.ts
@@ -5,22 +5,10 @@ export const config = {
   // exclude hongkong cause it's not supported by OpenAI
   regions: [
     "sin1",
-    "cdg1",
-    "arn1",
-    "dub1",
-    "lhr1",
-    "iad1",
-    "sfo1",
-    "pdx1",
-    "cle1",
-    "gru1",
+    "hkg1",
     "hnd1",
-    "icn1",
     "kix1",
-    "bom1",
     "syd1",
-    "fra1",
-    "cpt1",
   ],
 };
 
